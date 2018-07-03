1decbf1acb63c19e76429dc4928ccb0c437a8414
The Chad <tehchizad@gmail.com>
2018-07-02 22:36:19 -0600

    idk

--- actions/salesforce.js
+++ actions/salesforce.js
@@ -1,39 +1,84 @@
-"use strict"
-
 const SF_CLIENT_ID =
-    "3MVG9AJuBE3rTYDg8cnWQK_PLHQW91c6wWl95D6Pn.3OuqzJ1GtdEkifzyrVjadN50rcBkA2O2BiUIIFwU7Sz", //process.env.SF_CLIENT_ID,
-  SF_CLIENT_SECRET = "3002508663153639280", //process.env.SF_CLIENT_SECRET,
-  SF_USER_NAME = "invitaeslack@taos.com.invitae", //process.env.SF_USER_NAME,
-  SF_ACCESS_TOKEN = "XR82pq6thJJSdlMbE2D76qa79",
-  SF_PASSWORD = "ypkHJrXX2nG*s9!" + SF_ACCESS_TOKEN, //process.env.SF_PASSWORD
-  SF_URL = "https://test.salesforce.com/services/oauth2/token",
-  SF_REDIRECT_URI = "http://localhost:3000/oauth/_callback";
+    '3MVG9AJuBE3rTYDg8cnWQK_PLHQW91c6wWl95D6Pn.3OuqzJ1GtdEkifzyrVjadN50rcBkA2O2BiUIIFwU7Sz', //process.env.SF_CLIENT_ID,
+  SF_CLIENT_SECRET = '3002508663153639280', //process.env.SF_CLIENT_SECRET,
+  SF_USER_NAME = 'invitaeslack@taos.com.invitae', //process.env.SF_USER_NAME,
+  SF_ACCESS_TOKEN = 'XR82pq6thJJSdlMbE2D76qa79',
+  SF_PASSWORD = 'ypkHJrXX2nG*s9!' + SF_ACCESS_TOKEN, //process.env.SF_PASSWORD
+  SF_REDIRECT_URI = 'http://localhost:3000/oauth/_callback',
+  SF_URL = 'https://test.salesforce.com/services/oauth2/token',
+  POST_BODY =
+    'grant_type=password&client_id=3MVG9AJuBE3rTYDg8cnWQK_PLHQW91c6wWl95D6Pn.3OuqzJ1GtdEkifzyrVjadN50rcBkA2O2BiUIIFwU7Sz&client_secret=3002508663153639280&username=invitaeslack@taos.com.invitae&password=ypkHJrX$*X2nG$_Q7RJRH*s9!XR82pq6thJJSdlMbE2D76qa79',
+  POST_TYPE = 'application/x-www-form-urlencoded';
 
-let nforce = require("nforce");
+// // botkit format for user auth
+// let nforce = require('nforce'),
+//   org = nforce.createConnection({
+//     clientId: SF_CLIENT_ID,
+//     clientSecret: SF_CLIENT_SECRET,
+//     redirectUri: SF_REDIRECT_URI,
+//     mode: 'single',
+//     autoRefresh: true
+//   });
 
-// SF_CLIENT_ID = process.env.SF_CLIENT_ID,
-// SF_CLIENT_SECRET = process.env.SF_CLIENT_SECRET,
-// SF_USER_NAME = process.env.SF_USER_NAME,
-// SF_PASSWORD = process.env.SF_PASSWORD;
+// let login = () => {
+//   org.authenticate({ username: SF_USER_NAME, password: SF_PASSWORD }, err => {
+//     if (err) {
+//       console.error('Authentication error');
+//       console.error(err);
+//     } else {
+//       console.log('Authentication successful');
+//     }
+//   });
+// };
+function auth() {
+  var request = require('request'),
+  qs = require('querystring'), 
+  oauth =
+  { callback: SF_REDIRECT_URI  , consumer_key: SF_CLIENT_ID  , consumer_secret: SF_CLIENT_SECRET  }, 
+  url = SF_URL
+;
+request.post({url:url, oauth:oauth}, function (e, r, body) {
+// Ideally, you would take the body in the response
+// and construct a URL that a user clicks on (like a sign in button).
+// The verifier is only available in the response after a user has
+// verified with twitter that they are authorizing your app.
 
-let org = nforce.createConnection({
-  clientId: SF_CLIENT_ID,
-  clientSecret: SF_CLIENT_SECRET,
-  redirectUri: "http://localhost:3000/oauth/_callback",
-  mode: "single",
-  autoRefresh: true
-});
+// step 2
+var req_data = qs.parse(body)
+var uri = 'https://api.twitter.com/oauth/authenticate'
+  + '?' + qs.stringify({oauth_token: req_data.oauth_token})
+// redirect the user to the authorize uri
 
-let login = () => {
-  org.authenticate({ username: SF_USER_NAME, password: SF_PASSWORD }, err => {
-    console.error("Authenticating...");
-    if (err) {
-      console.log("Failed: ")
-      console.error(err);
-    } else {
-      console.log("Successful.");
+// step 3
+// after the user is redirected back to your server
+var auth_data = qs.parse(body)
+  , oauth =
+    { consumer_key: CONSUMER_KEY
+    , consumer_secret: CONSUMER_SECRET
+    , token: auth_data.oauth_token
+    , token_secret: req_data.oauth_token_secret
+    , verifier: auth_data.oauth_verifier
     }
-  });
-};
-
-login();
+  , url = 'https://api.twitter.com/oauth/access_token'
+  ;
+request.post({url:url, oauth:oauth}, function (e, r, body) {
+  // ready to make signed requests on behalf of the user
+  var perm_data = qs.parse(body)
+    , oauth =
+      { consumer_key: CONSUMER_KEY
+      , consumer_secret: CONSUMER_SECRET
+      , token: perm_data.oauth_token
+      , token_secret: perm_data.oauth_token_secret
+      }
+    , url = 'https://api.twitter.com/1.1/users/show.json'
+    , qs =
+      { screen_name: perm_data.screen_name
+      , user_id: perm_data.user_id
+      }
+    ;
+  request.get({url:url, oauth:oauth, qs:qs, json:true}, function (e, r, user) {
+    console.log(user)
+  })
+})
+})
+module.exports.auth = auth
--- app.js
+++ app.js
@@ -1,25 +1,63 @@
-const SlackBot = require('slackbots'),
-  actions = require('./actions/chat')
+'use strict';
+
+let http = require('http'),
+  actions = require('./actions/chat'),
+  salesforce = require('./actions/salesforce'),
+  SlackBot = require('slackbots');
+
+const SLACK_BOT_TOKEN =
+  'xoxb-390227820583-389404942256-OQUJiVYWFrlN56RpzsOOHJ0N'; //process.env.SLACK_BOT_TOKEN;
+
+// // botkit version
+// let Botkit = require('botkit'),
+//   formatter = require('./actions/slack-formatter'),
+//   salesforce = require('./actions/salesforce'),
+//   controller = Botkit.slackbot(),
+//   bot = controller.spawn({
+//     token: SLACK_BOT_TOKEN
+//   });
+
+// bot.startRTM(err => {
+//   if (err) {
+//     throw new Error('Could not connect to Slack');
+//   }
+// });
+
+// controller.hears(
+//   ['help'],
+//   'direct_message,direct_mention,mention',
+//   (bot, message) => {
+//     bot.reply(message, {
+//       text: `You can ask me things like:
+//     "Search account Acme" or "Search Acme in acccounts"
+//     "Search contact Lisa Smith" or "Search Lisa Smith in contacts"
+//     "Search opportunity Big Deal"
+//     "Create contact"
+//     "Create case"`
+//     });
+//   }
+// );
 
 const bot = new SlackBot({
-  token: 'xoxb-390227820583-389404942256-OQUJiVYWFrlN56RpzsOOHJ0N',
+  token: SLACK_BOT_TOKEN,
   name: 'codette'
-})
+});
 
 // Start handler
 bot.on('start', () => {
-  actions.postText('#blessed')
-})
+  actions.postText('#blessed');
+  salesforce.auth();
+});
 
 // Message handling
-bot.on('message', (data) => {
+bot.on('message', data => {
   if (data.type !== 'message') {
-    return
+    return;
   }
-  actions.parseRequest(data.text)
-})
+  actions.parseRequest(data.text);
+});
 
 // Error handling
-bot.on('error', (err) => console.log(err))
+bot.on('error', err => console.log(err));
 
-module.exports.bot = bot
+module.exports.bot = bot;
